97e07f833895a83d9ec910fecdc5c462c3c4f80e
teo <teopeleato@gmail.com>
2018-09-27 19:53:56 +0200

    yargs parametros

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
 console.log('iniciando aplicacion de notas')
-const notas = require('./notes.js')
+const notes = require('./notes.js')
+const { argv } = require('yargs')
 
-notas.addNote()
-notas.removeNote(7)
+console.log(argv)
+
+const comando = argv._[0]
+
+// const param = process.argv[2]
+
+switch (comando) {
+  case 'addNote':
+    notes.addNote(argv.title, argv.body)
+    break
+  case 'removeNote':
+    console.log('Eliminando una nota...')
+    break
+  case 'readNote':
+    console.log('Leyendo una nota...')
+    break
+  case 'listNotes':
+    console.log('Listando notas...')
+    break
+  default:
+    console.log('Comando desconocido')
+    break
+}
--- notes.js
+++ notes.js
@@ -1,16 +1,25 @@
 console.log('Modulo de notas cargado')
 
-const addNote = () => {
-  console.log('Nueva nota')
-  return 'Nueva nota'
+const addNote = (title, body) => {
+  console.log('Nota añadida: ', title, body)
 }
 
 const removeNote = (idNota) => {
-  console.log(`Nota ${idNota} eliminada`)
+  console.log(`Nota ${idNota} eliminadas`)
   return 'Nota borrada'
+}
+
+const readNote = (idNota) => {
+  return 'Nota leida'
+}
+
+const listNotes = () => {
+  return 'Listar'
 }
 
 module.exports = {
   addNote,
-  removeNote
+  removeNote,
+  readNote,
+  listNotes
 }
